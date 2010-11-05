3b18dd6622fe08c0ff79f28312760f04d03cadf6
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-11-05 23:53:25 +0100

    start handling cmdline, panic for now

--- init.c
+++ init.c
@@ -1,13 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/mount.h>
 #include <sys/wait.h>
 
+#define MAX_INIT_PATH_SIZE 25
+
 #define DEFAULT_ROOT_PATH "/dev/lvm/gentoo"
 #define DEFAULT_INIT_PATH "/sbin/init"
 #define ROOT_FILESYSTEM_TYPE "ext4"
 #define OUTPUT stdout
+
+typedef struct {
+	char init[MAX_INIT_PATH_SIZE+1]; /* Or we'll have a leak */
+	char * root;
+} cmdline;
+
+bool
+is_blank(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' || c == EOF);
+}
+
+char *
+get_value(FILE * f) {
+	char * path = (char *) malloc(11*sizeof(char)); /* /sbin/init size */
+	int i = 0;
+	char c;
+	while (!is_blank(c = fgetc(f)))
+	{
+		path[i] = c;
+		if (++i%10 == 1)
+			path = (char *) realloc(path, (i+10) * sizeof(char));
+	}
+	if (i == 0)
+	{
+		free (path);
+		return NULL;
+	}
+	path[i] = '\0';
+	return path;
+}
+
+cmdline
+parse_kernel_cmdline()
+{
+	cmdline paths;
+	paths.root = NULL;
+	paths.init[0] = '\0';
+	FILE * f = fopen("/proc/cmdline", "r");
+	if (!f)
+		return paths;
+	char c;
+	while ((c = fgetc(f)) != EOF)
+	{
+		if (c == 'i' && fgetc(f) == 'n' && fgetc(f) == 'i' && fgetc(f) == 't' && fgetc(f) == '=')
+		{
+			char * tmp = get_value(f);
+			if (!tmp)
+				continue;
+			else if (strlen(tmp) > MAX_INIT_PATH_SIZE)
+				free(tmp);
+			else
+				strcpy(paths.init, tmp);
+		}
+		else if (c == 'r' && fgetc(f) != 'o' && fgetc(f) == 'o' && fgetc(f) == 't' && fgetc(f) == '=')
+			paths.root = get_value(f);
+	}
+	fclose(f);
+	return paths;
+}
 
 int
 init()
@@ -40,20 +105,6 @@ init()
 	execl("/sbin/lvm", "/sbin/lvm", "vgchange", "-ay", NULL);
 
 	return 0;
-}
-
-char *
-get_root_path()
-{
-	/* TODO: handle kernel command line */
-	return DEFAULT_ROOT_PATH;
-}
-
-char *
-get_init_path()
-{
-	/* TODO: handle kernel command line */
-	return DEFAULT_INIT_PATH;
 }
 
 void
@@ -98,18 +149,21 @@ main()
 	pid_t pid = 0;
 	if (! (pid = fork()))
 		return init(); /* Do the first part of the basic stuff in a fork */
-	char * root_path = get_root_path();
-	char * init_path = get_init_path();
 	waitpid(pid, NULL, 0); /* Wait for the lvm volume groups to be activated */
+	cmdline paths = parse_kernel_cmdline();
+	char * root_path = paths.root ? paths.root : DEFAULT_ROOT_PATH;
+	char * init_path = (paths.init[0] != '\0') ? paths.init : DEFAULT_INIT_PATH;
 	fprintf(OUTPUT, "Mounting root device: %s...\n", root_path);
 	/* Mount / */
 	mount(root_path, "/root", ROOT_FILESYSTEM_TYPE, MS_RDONLY, NULL);
+	if (paths.root)
+		free (paths.root);
 	fprintf(OUTPUT, "Unmounting /sys and /proc ...\n");
 	/* Cleanup */
 	umount("/sys");
 	umount("/proc");
 	rm_rf ("/dev");
-	fprintf(OUTPUT, "### resuming normal boot ###\n");
+	fprintf(OUTPUT, "Resuming normal boot with init: %s...\n", init_path);
 	switch_root();
 	execl(init_path, init_path, NULL);
 }
