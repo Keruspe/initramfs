7d13e5354065cc18a09055ff15575309936dcef7
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-11-20 21:32:56 +0100

    add tool to encrypt data
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: init 
+all: init encrypt
 
 init: init.c
 	gcc init.c -Ofast -march=native -Wall -W -Wextra -Werror -std=gnu99 -o init -lcryptsetup -ldevmapper -lgcrypt -lgpg-error -luuid -ludev -lrt -Wl,-O3 -Wl,--as-needed -static
+
+encrypt: encrypt.c
+	gcc encrypt.c -Ofast -march=native -Wall -W -Wextra -Werror -std=gnu99 -o encrypt -lgcrypt -Wl,-O3 -Wl,--as-needed
 
 clean:
 	rm -f init
---
+++ encrypt.c
@@ -0,0 +1,79 @@
+#include <gcrypt.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+#define CIPHER GCRY_CIPHER_AES256
+
+int
+main (int argc, char *argv[])
+{
+    const char *output_file = NULL;
+    FILE *in = NULL;
+
+    switch (argc)
+    {
+    case 3:
+        in = fopen (argv[1], "r");
+    case 2:
+        output_file = argv[argc - 1];
+        break;
+    default:
+        fprintf (stderr, "Need either one or two arguments: <output file> or <input file> <output file>\n");
+        return 1;
+    }
+
+    if (!in)
+    {
+        if (isatty (fileno (stdin)))
+        {
+            fprintf (stderr, "You need to pipe content if you only specify an output file or if input file is not readable\n");
+            return 1;
+        }
+        in = stdin;
+    }
+
+    size_t len = 0;
+    size_t total_size = 4096;
+    char *content = (char *) malloc (total_size * sizeof (char));
+    for (char c = fgetc (in); c != EOF; c = fgetc (in))
+    {
+        if ((len % 4096) == 5)
+        {
+            total_size += 4096;
+            content = (char *) realloc (content, total_size * sizeof (char));
+        }
+        content[++len + 4] = c;
+    }
+    char tmp = content[5];
+    sprintf (content, "%5lu", len);
+    content[5] = tmp;
+
+    size_t keylen = gcry_cipher_get_algo_keylen (CIPHER);
+    size_t blklen = gcry_cipher_get_algo_blklen (CIPHER);
+    char *key = getpass ("Passphrase: ");
+
+    char *iv = (char *) malloc (blklen * sizeof (char));
+    srand (time (NULL));
+    for (size_t i = 0; i < blklen; ++i)
+        iv[i] = (char) rand ();
+ 
+    gcry_cipher_hd_t handle;
+    gcry_cipher_open (&handle, CIPHER, GCRY_CIPHER_MODE_CBC, GCRY_CIPHER_SECURE|GCRY_CIPHER_CBC_CTS);
+    gcry_cipher_setkey (handle, key, keylen);
+    gcry_cipher_setiv (handle, iv, blklen);
+    gcry_cipher_encrypt (handle, content, total_size, NULL, 0);
+
+    FILE *out = fopen (output_file, "w+");
+    fprintf (out, "%s\n", iv);
+    for (size_t i = 0; i < total_size; ++i)
+        fputc (content[i], out);
+    fclose (out);
+
+    gcry_cipher_close (handle);
+    free (key);
+    free (iv);
+    free (content);
+
+    return 0;
+}
