2ef2d6aa11df1d0f498ab17ac6ac2666b6495b9c
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-11-24 17:13:02 +0100

    decrypt: externalize decrypt-util
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- decrypt.c
+++ decrypt.c
@@ -4,37 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define CIPHER GCRY_CIPHER_AES256
-
-static void
-decrypt (FILE            *in,
-         FILE            *out,
-         gcry_cipher_hd_t handle,
-         size_t           blklen)
-{
-    char *iv = (char *) malloc (blklen * sizeof (char));
-    fread (iv, blklen, 1, in);
-    gcry_cipher_reset (handle);
-    gcry_cipher_setiv (handle, iv, blklen);
-    free (iv);
-
-    fseek (in, 0, SEEK_END);
-    size_t len = ftell (in);
-    fseek (in, 0, SEEK_SET);
-
-    char *raw_content = (char *) malloc ((len + 1) * sizeof (char));
-    fread (raw_content, len, 1, in);
-    fclose (in);
-
-    char *content = raw_content + blklen;
-    len -= blklen;
-
-    gcry_cipher_decrypt (handle, content, len, NULL, 0);
-    fwrite (content, len, 1, out);
-    fclose (out);
-
-    free (raw_content);
-}
+#include "decrypt-util.c"
 
 int
 main (int argc, char *argv[])
