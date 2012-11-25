0fa0dc7b609147a8c4948f28d29eb4e0b2fcfaa0
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-11-25 18:14:44 +0100

    make it work
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- init.c
+++ init.c
@@ -10,37 +10,44 @@
 #define LUKS_HEADER_ENCRYPTED LUKS_HEADER_PLAIN ".aes"
 #define LUKS_PASSFILE_PLAIN "/root/luks_passfile"
 #define LUKS_PASSFILE_ENCRYPTED LUKS_PASSFILE_PLAIN ".aes"
-#define LUKS_DATA_DEVICE "/dev/sda4"
+#define LUKS_DATA_DEVICE "/dev/sdb"
 #define LUKS_VOLUME "root-luks"
 
 #define ROOT_PATH "/dev/sda5"
 #define INIT_PATH "/sbin/init"
 #define FILESYSTEM_TYPE "ext4"
 
-#if 0
 #include "decrypt-util.c"
-#endif
 
 #define AES_DECRYPT(in, out) \
     decrypt (fopen (in, "rb"), \
              fopen (out, "wb"), \
              handle, \
-             blklen)
+             blklen);
 
-int
-main (void)
+#define UMOUNT(dir) \
+    umount (dir); \
+    rmdir (dir);
+
+static void
+basic_mounts ()
 {
     mount ("none", "/proc", "proc", 0, NULL);
     mount ("none", "/sys", "sysfs", 0, NULL);
     mount ("none", "/dev", "devtmpfs", 0, NULL);
+}
 
-#if 0
+int
+main (void)
+{
+    basic_mounts ();
+
     #include "crypt-init.c"
 
     AES_DECRYPT (LUKS_HEADER_ENCRYPTED,
-                 LUKS_HEADER_PLAIN);
+                 LUKS_HEADER_PLAIN)
     AES_DECRYPT (LUKS_PASSFILE_ENCRYPTED,
-                 LUKS_PASSFILE_PLAIN);
+                 LUKS_PASSFILE_PLAIN)
 
     #include "crypt-deinit.c"
 
@@ -51,20 +58,21 @@ main (void)
     crypt_set_password_retry(cd, 1);
     crypt_activate_by_keyfile_offset(cd, LUKS_VOLUME, 0, LUKS_PASSFILE_PLAIN, 0, 0, 0);
     crypt_free (cd);
-#endif
 
     mount (ROOT_PATH, "/root", FILESYSTEM_TYPE, MS_RDONLY, NULL);
     
-    umount ("/proc");
-    umount ("/sys");
-    umount ("/dev");
+    UMOUNT ("/proc")
+    UMOUNT ("/sys")
+    UMOUNT ("/dev")
     
-    if (chdir ("/root") != 0) return -1;
+    chdir ("/root");
     mount (".", "/", NULL, MS_MOVE, NULL);
-    if (chroot (".") != 0) return -1;
-    if (chdir ("/") != 0) return -1;
+    chroot (".");
+    chdir ("/");
+
+    basic_mounts ();
     
-    //mount ("/dev/mapper/root-luks", "/mnt/usbstick", FILESYSTEM_TYPE, MS_RDONLY, NULL);
+    mount ("/dev/mapper/root-luks", "/mnt/usbstick", FILESYSTEM_TYPE, MS_RDONLY, NULL);
 
     execl (INIT_PATH, INIT_PATH, NULL);
 }
