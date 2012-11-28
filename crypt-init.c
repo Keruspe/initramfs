fbb576df0bc21e7e47dc1d62adee18320552b5ee
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-11-28 21:22:55 +0100

    fix gcrypt stuff
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- crypt-init.c
+++ crypt-init.c
@@ -10,9 +10,8 @@
     gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
 
     size_t blklen = gcry_cipher_get_algo_blklen (CIPHER);
-    size_t keylen = gcry_cipher_get_algo_keylen (CIPHER);
-    char *key = "coucou"; //getpass ("Passphrase: ");
+    char *key = getpass ("Passphrase: ");
 
     gcry_cipher_hd_t handle;
     gcry_cipher_open (&handle, CIPHER, GCRY_CIPHER_MODE_CBC, GCRY_CIPHER_SECURE|GCRY_CIPHER_CBC_CTS);
-    gcry_cipher_setkey (handle, key, keylen);
+    gcry_cipher_setkey (handle, key, strlen (key));
